13d5cc3f8cc06acbf328f47fb9926a20d66be6e5
Aasif Ali <aa4571756@gmail.com>
2023-08-03 08:44:40 +0500

    Customize Toast

---
+++ app/src/main/java/com/example/practice/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.practice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.content.res.ResourcesCompat
+import com.example.practice.databinding.ActivityMainBinding
+import www.sanju.motiontoast.MotionToast
+import www.sanju.motiontoast.MotionToastStyle
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.button.setOnClickListener{
+            MotionToast.createToast(this,
+                "Hurray success 😍",
+                "Upload Completed successfully!",
+                MotionToastStyle.SUCCESS,
+                MotionToast.GRAVITY_BOTTOM,
+                MotionToast.LONG_DURATION,
+                ResourcesCompat.getFont(this, www.sanju.motiontoast.R.font.helvetica_regular))
+        }
+    }
+}
